a1a90ee57231cd67106ed77f3a322f42130b3f93
kevin-nduati <crtskvn@gmail.com>
2022-07-04 23:48:26 +0300

    CSS

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// typing animation
+var typed = new Typed(".typing", {
+    // strings:['ggh', 'Machine Learning Engineer', 'Data Scientist', 'Big Data Architect'],
+    // typeSpeed: 100,
+    // BackSpeed:60,
+    // loop:true
+})
+
+        
